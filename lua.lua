b3149f3b42da2c8643e47567229496fdad836fcd
Ckituzer <83168446+RawLunaHub@users.noreply.github.com>
2023-05-19 13:18:09 +0300

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,2 +1,104 @@
 script.Name = "ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ"
-warn("idk")
+
+local function hasMethods(methods)
+    for name in pairs(methods) do
+        if not environment[name] then
+            return false
+        end
+    end
+
+    return true
+end
+
+local function useMethods(module)
+    for name, method in pairs(module) do
+        if method then
+            environment[name] = method
+        end
+    end
+end
+local globalMethods = {
+    checkCaller = checkcaller,
+    newCClosure = newcclosure,
+    hookFunction = hookfunction or detour_function,
+    getGc = getgc or get_gc_objects,
+    getInfo = debug.getinfo or getinfo,
+    getSenv = getsenv,
+    getMenv = getmenv or getsenv,
+    getContext = getthreadcontext or get_thread_context or (syn and syn.get_thread_identity),
+    getConnections = get_signal_cons or getconnections,
+    getScriptClosure = getscriptclosure or get_script_function,
+    getNamecallMethod = getnamecallmethod or get_namecall_method,
+    getCallingScript = getcallingscript or get_calling_script,
+    getLoadedModules = getloadedmodules or get_loaded_modules,
+    getConstants = debug.getconstants or getconstants or getconsts,
+    getUpvalues = debug.getupvalues or getupvalues or getupvals,
+    getProtos = debug.getprotos or getprotos,
+    getStack = debug.getstack or getstack,
+    getConstant = debug.getconstant or getconstant or getconst,
+    getUpvalue = debug.getupvalue or getupvalue or getupval,
+    getProto = debug.getproto or getproto,
+    getMetatable = getrawmetatable or debug.getmetatable,
+    getHui = get_hidden_gui or gethui,
+    setClipboard = setclipboard or writeclipboard,
+    setConstant = debug.setconstant or setconstant or setconst,
+    setContext = setthreadcontext or set_thread_context or (syn and syn.set_thread_identity),
+    setUpvalue = debug.setupvalue or setupvalue or setupval,
+    setStack = debug.setstack or setstack,
+    setReadOnly = setreadonly or (make_writeable and function(table, readonly) if readonly then make_readonly(table) else make_writeable(table) end end),
+    isLClosure = islclosure or is_l_closure or (iscclosure and function(closure) return not iscclosure(closure) end),
+    isReadOnly = isreadonly or is_readonly,
+    isXClosure = is_synapse_function or issentinelclosure or is_protosmasher_closure or is_sirhurt_closure or iselectronfunction or istempleclosure or checkclosure,
+    hookMetaMethod = hookmetamethod or (hookfunction and function(object, method, hook) return hookfunction(getMetatable(object)[method], hook) end),
+    readFile = readfile,
+    writeFile = writefile,
+    makeFolder = makefolder,
+    isFolder = isfolder,
+    isFile = isfile,
+}
+local oldGetUpvalue = globalMethods.getUpvalue
+local oldGetUpvalues = globalMethods.getUpvalues
+globalMethods.getUpvalue = function(closure, index)
+    if type(closure) == "table" then
+        return oldGetUpvalue(closure.Data, index)
+    end
+
+    return oldGetUpvalue(closure, index)
+end
+
+globalMethods.getUpvalues = function(closure)
+    if type(closure) == "table" then
+        return oldGetUpvalues(closure.Data)
+    end
+
+    return oldGetUpvalues(closure)
+end
+environment.hasMethods = hasMethods
+
+if getConnections then 
+    for __, connection in pairs(getConnections(game:GetService("ScriptContext").Error)) do
+
+        local conn = getrawmetatable(connection)
+        local old = conn and conn.__index
+        
+        if PROTOSMASHER_LOADED ~= nil then setwriteable(conn) else setReadOnly(conn, false) end
+        
+        if old then
+            conn.__index = newcclosure(function(t, k)
+                if k == "Connected" then
+                    return true
+                end
+                return old(t, k)
+            end)
+        end
+
+        if PROTOSMASHER_LOADED ~= nil then
+            setReadOnly(conn)
+            connection:Disconnect()
+        else
+            setReadOnly(conn, true)
+            connection:Disable()
+        end
+    end
+end
+useMethods(globalMethods)
